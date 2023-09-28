2809131fff606531cc0c1420155eb82f2606dc08
kim pahud <Kphd64@gmail.com>
2023-09-28 10:44:46 -0400

    AJOUT EURO

--- outilmonnaie/Program.cs
+++ outilmonnaie/Program.cs
@@ -12,17 +12,39 @@ namespace outilmonnaie
         {
             Console.WriteLine("entrez le montant a convertir");
             double montant = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("ce montant est t'il en us ou en can");
+            Console.WriteLine("ce montant est t'il en us, en can ou en euro");
             string type = Console.ReadLine();
-            if (type == "us")
+            Console.WriteLine("En quoi voulez vous convertir votre montant");
+            string type2 = Console.ReadLine();
+            if (type == "can" && type2 == "us")
             {
                 double dollus = montant * 1.3496;
                 Console.WriteLine(montant + " dollards can donne" + dollus + "en dollards us");
             }
-            else 
+            if (type == "can" && type2 == "euro")
             {
-                double dollcan = montant / 1.3496;
-                Console.WriteLine(montant + " dollards us donne" + dollcan + "en dollards can");
+                double dolleu = montant * 1.42;
+                Console.WriteLine(montant + " dollards can donne" + dolleu + "en dollards euro");
+            }
+            if (type == "us" && type2 == "euro")
+            {
+                double dolleu = montant * 0.95;
+                Console.WriteLine(montant + " dollards us donne" + dolleu + "en dollards euro");
+            }
+            if (type == "us" && type2 == "can")
+            {
+                double dollca = montant / 1.3496;
+                Console.WriteLine(montant + " dollards us donne" + dollca + "en dollards can");
+            }
+            if (type == "euro" && type2 == "us")
+            {
+                double dollus = montant / 0.95;
+                Console.WriteLine(montant + " dollards can donne" + dollus + "en dollards euro");
+            }
+            if (type == "euro" && type2 == "can")
+            {
+                double dollca = montant * 1.42;
+                Console.WriteLine(montant + " dollards can donne" + dollca + "en dollards euro");
             }
             Console.ReadKey();
         }       
