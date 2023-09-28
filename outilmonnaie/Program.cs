ddbcf3f36fa654d2f961d9766653c12fe69f86fa
kim pahud <Kphd64@gmail.com>
2023-09-28 10:12:40 -0400

    prog can us

---
+++ outilmonnaie/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace outilmonnaie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entrez le montant a convertir");
+            double montant = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ce montant est t'il en us ou en can");
+            string type = Console.ReadLine();
+            if (type == "us")
+            {
+                double dollus = montant * 1.3496;
+                Console.WriteLine(montant + " dollards can donne" + dollus + "en dollards us");
+            }
+            else 
+            {
+                double dollcan = montant / 1.3496;
+                Console.WriteLine(montant + " dollards us donne" + dollcan + "en dollards can");
+            }
+            Console.ReadKey();
+        }       
+    }
+}
